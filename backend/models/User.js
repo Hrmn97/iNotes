ea5f8559308d86a31db5a5e0f7562f2a1d8bb8ab
harman singh <singh9765h@gmail.com>
2023-01-07 02:19:46 +0530

    Validated the data for each registery

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,23 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        reuired: true
-    },
-    email:{
-        type: String,
-        reuired: true,
-        unique: true
-    },
-    password:{
-        type: String,
-        default: 'General'
-    },
-    date:{
-        type: Date,
-        default: Date.now
-    }
-})
-
-module.exports = mongoose.model('users',UserSchema)
+  name: {
+    type: String,
+    reuired: true,
+  },
+  email: {
+    type: String,
+    reuired: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    default: "General",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const User = mongoose.model("users", UserSchema);
+User.createIndexes();
+module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
 //create a user using: POST "/api/auth". Doesn't require Authentication
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
-});
+router.post(
+  "/",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 3,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => console.log(err));
+    res.json({ error: "Email already registered" });
+  }
+);
 
 module.exports = router;
