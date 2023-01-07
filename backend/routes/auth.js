6b9baa7a8770af502c25aa254044d318bb182df5
harman singh <singh9765h@gmail.com>
2023-01-07 20:08:59 +0530

    Data retrieval of logged in user from database with creds.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,9 +4,11 @@ const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 var bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
+var fetchuser = require("../middleware/fetchuser");
 
 const JWT_SECRET = "HarmanisBADBOY";
 
+//ROUTE 1
 //create a user using: POST "/api/auth/createuser". Doesn't require Login
 
 router.post(
@@ -48,9 +50,60 @@ router.post(
       res.json({ authtoken });
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Error occured");
+      res.status(500).send("Server error");
     }
   }
 );
 
+//ROUTE 2
+//authenticate a user: POST "/api/auth/login" . Doesn't require login
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    //If there are errors, Bad Request generated
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: "Incorrect credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res.status(400).json({ error: "Incorrect credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+//ROUTE 3
+//Get loggedin user details: POST "/api/auth/getuser". Require Login
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    let userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+});
 module.exports = router;
